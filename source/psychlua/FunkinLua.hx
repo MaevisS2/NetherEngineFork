ac26368e9884643cc43924015ced30ccadc5b794
Maevi <84648753+MaevisS2@users.noreply.github.com>
2023-08-20 13:04:23 -0300

    Its our engine!

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -132,7 +132,7 @@ class FunkinLua {
 		set('rating', 0);
 		set('ratingName', '');
 		set('ratingFC', '');
-		set('version', MainMenuState.psychEngineVersion.trim());
+		set('version', MainMenuState.netherEngineVersion.trim());
 
 		set('inGameOver', false);
 		set('mustHitSection', false);
