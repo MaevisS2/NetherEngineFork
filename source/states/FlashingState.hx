d51ccd9023fdf384e7e6bccb2bf265573d76a152
Marshverso <115442552+Marshverso@users.noreply.github.com>
2023-08-20 12:00:44 -0300

    Update FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -19,11 +19,9 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"WARNING!\n
+			MOD HAS FLASHING LIGHTS!\n
+			Press Enter Disables or Press Space to Continue.",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
